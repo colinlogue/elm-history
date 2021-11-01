4d468080d01d3ab6a8d49356b9328e18e94651dd
Colin Logue <email@colinlogue.me>
2021-11-01 09:26:02 -0700

    add doc comments

--- src/History.elm
+++ src/History.elm
@@ -2,7 +2,7 @@ module History exposing (..)
 
 import History.Advanced as Advanced
 
-{-| Data structure for keeping 
+{-| Data structure for keeping a linear edit history.
 -}
 type alias History state = Advanced.History state state
 
@@ -26,17 +26,63 @@ new change, the undone state will not be recoverable.
 push : state -> History state -> History state
 push = Advanced.push
 
-{-| Move back one 
+{-| Move back one step in the history. Undoing from the initial state
+returns the history unchanged.
+
+  begin 0
+    |> push 1
+    |> push 2
+    |> undo
+    |> history
+  -- [0,1]
+
+  undo (begin 0) == begin 0
+  -- True
+
 -}
 undo : History state -> History state
 undo = Advanced.undo
 
+{-| Move forward one step in the history. Redoing from a state with no
+future path returns the history unchanged.
+
+  begin 0
+    |> push 1
+    |> undo
+    |> redo
+    |> history
+  -- [0,1]
+
+  begin 0
+    |> push 1
+    |> push 2
+    |> redo
+    |> history
+  -- [0,1,2]
+
+-}
 redo : History state -> History state
 redo = Advanced.redo
 
+{-| Get the current state in the history.
+
+  begin 0
+    |> current
+  -- 0
+
+  begin 0
+    |> push 1
+    |> push 2
+    |> undo
+    |> current
+  -- 1
+
+-}
 current : History state -> state
 current = Advanced.current
 
+{-| Get the history up to and including the current state as a list.
+-}
 history : History state -> List state
 history save =
   List.foldl
